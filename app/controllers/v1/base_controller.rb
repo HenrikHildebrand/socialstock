53c04b3589eb7e1cdf24412fc0aa50debe8cc161
magnusbrattlof <brattlof.magnus@gmail.com>
2020-03-19 21:54:46 +0100

    added token based authorization support

--- app/controllers/v1/base_controller.rb
+++ app/controllers/v1/base_controller.rb
@@ -1,6 +1,5 @@
 class V1::BaseController < ActionController::API
     include ActionController::MimeResponds
-
-    before_action :authenticate_user!
-
+    # before_action :authenticate_user!
+    acts_as_token_authentication_handler_for User
 end
